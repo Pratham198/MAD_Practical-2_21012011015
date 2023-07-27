38834198808ac9c3e2c561f0a29150d5007de74d
MY GPSC <danipratham4581@gmail.com>
2023-07-27 08:53:14 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mad_practical_2_21012011015/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.mad_practical_2_21012011015
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    val TAG="MainActivity"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        showMessage("onCreate() is called")
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        showMessage("onStart() is called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        showMessage("onResume() is called")
+    }
+    fun showMessage(message: String)
+    {
+        Log.i(TAG, message )
+        Toast.makeText(this,message, Toast.LENGTH_SHORT).show()
+    }
+}
